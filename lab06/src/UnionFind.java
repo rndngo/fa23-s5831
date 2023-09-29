456c4f558790838fe2492ee3d88a1ade380400ee
Ryan-Nguyen <rnd.ngo@berkeley.edu>
2023-09-28 20:46:13 -0700

    Finished

--- lab06/src/UnionFind.java
+++ lab06/src/UnionFind.java
@@ -39,9 +39,9 @@ public class UnionFind {
     /* Returns true if nodes/vertices V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
         // TODO: YOUR CODE HERE
-        return (parent(v1) != -1 && parent(v2) != -1 && (find(v1) == find(v2) || parent(v1) == parent(v2)));
+        return (parent(v1) != -1 && parent(v2) != -1 && parent(v1) == parent(v2));
     }
-//(find(v1) == find(v2) ||
+    //(find(v1) == find(v2) ||
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. If invalid items are passed into this
        function, throw an IllegalArgumentException. */
@@ -65,51 +65,39 @@ public class UnionFind {
        already connected should not change the structure. */
     public void union(int v1, int v2) {
         // TODO: YOUR CODE HERE
-        if (!connected(v1, v2) && v1 !=v2) {
-            if (sizeOf(v1) == sizeOf(v2)) {
-                data[find(v2)] = -1 * (sizeOf(v1) + sizeOf(v2));
-                data[find(v1)] = v2;
-            } else {
-                data[find(v1)] = -1 * (sizeOf(v1) + sizeOf(v2));
-                data[find(v2)] = v1;
+        if (v1 == v2) {
+            throw new IllegalArgumentException();
+        }
+        else if (parent(v1) == parent(v2)) {
+            if (sizeOf(parent(v1)) == sizeOf(parent(v2))) {
+                data[v2] = data[v2] + data[v1];
+                data[v1] = v2;
             }
-        } else {
-            return;
+            else {
+                data[v1] = data[v1] + data[v2];
+                data[v2] = v1;
+            }
+        }
+        else if (parent(v1) < 0 && parent(v2) < 0) {
+            if (sizeOf(parent(v1)) == sizeOf(parent(v2))) {
+                data[v2] = data[v2] + data[v1];
+                data[v1] = v2;
+            }
+            else {
+                data[v1] = data[v1] + data[v2];
+                data[v2] = v1;
+            }
+        }
+        else if (parent(v1) < 0) {
+            union(v1, parent(v2));
+        }
+        else if (parent(v2) < 0) {
+            union(parent(v1), v2);
+        }
+        else {
+            union(parent(v1),parent(v2));
         }
     }
-//        if (v1 == v2) {
-//            return;
-//        }
-//        else if (parent(v1) == parent(v2)) {
-//            if (sizeOf(parent(v1)) == sizeOf(parent(v2))) {
-//                data[v2] = data[v2] + data[v1];
-//                data[v1] = v2;
-//            }
-//            else {
-//                data[v1] = data[v1] + data[v2];
-//                data[v2] = v1;
-//            }
-//        }
-//        else if (parent(v1) < 0 && parent(v2) < 0) {
-//            if (sizeOf(parent(v1)) == sizeOf(parent(v2))) {
-//                data[v2] = data[v2] + data[v1];
-//                data[v1] = v2;
-//            }
-//            else {
-//                data[v1] = data[v1] + data[v2];
-//                data[v2] = v1;
-//            }
-//        }
-//        else if (parent(v1) < 0) {
-//            union(v1, parent(v2));
-//        }
-//        else if (parent(v2) < 0) {
-//            union(parent(v1), v2);
-//        }
-//        else {
-//            union(parent(v1),parent(v2));
-//        }
-
 
     /**
      * DO NOT DELETE OR MODIFY THIS, OTHERWISE THE TESTS WILL NOT PASS.
