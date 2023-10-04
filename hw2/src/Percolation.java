dbe68518a7c256e544d615f14ce0622a0a469008
Ryan-Nguyen <rnd.ngo@berkeley.edu>
2023-10-04 00:06:51 -0700

    Debugging

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -5,6 +5,7 @@ public class Percolation {
     private boolean[][] grid;
     private int opened;
     private WeightedQuickUnionUF GToU;
+    private WeightedQuickUnionUF isFullUnion;
     private final int water;
     private final int bottom;
     private boolean percolating;
@@ -19,6 +20,7 @@ public class Percolation {
         opened = 0;
         grid = new boolean[N][N];
         GToU = new WeightedQuickUnionUF(N * N + 2);
+        isFullUnion = new WeightedQuickUnionUF(N * N + 1);
         water = N * N;
         bottom = water + 1;
     }
@@ -59,8 +61,9 @@ public class Percolation {
             int otherCol = col + (int) xyOffset[1];
             if (otherRow < 0) { // we are touching water
                 GToU.union(water, center);
+                isFullUnion.union(water, center);
             }
-            if (otherRow >= dimension && !percolates()) { // we are touching the bottom
+            if (otherRow >= dimension) { // we are touching the bottom
                 GToU.union(center, bottom);
             }
 
@@ -68,6 +71,7 @@ public class Percolation {
             if (validBounds(otherRow, otherCol) && isOpen(otherRow, otherCol)) {
                 int x = convertGTU(otherRow, otherCol);
                 GToU.union(x , center);
+                isFullUnion.union(x, center);
             }
         }
 
@@ -88,7 +92,7 @@ public class Percolation {
         }
 
         int x = convertGTU(row, col);
-        return GToU.connected(x, water); // checks if the coordinate is connected to water.
+        return isFullUnion.connected(x, water); // checks if the coordinate is connected to water.
     }
 
     public int numberOfOpenSites() {
