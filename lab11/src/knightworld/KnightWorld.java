8bc65be69827930eb48c174c95fad29dc16c3278
Ryan-Nguyen <rnd.ngo@berkeley.edu>
2023-11-04 00:54:09 -0700

    Done

--- lab11/src/knightworld/KnightWorld.java
+++ lab11/src/knightworld/KnightWorld.java
@@ -2,6 +2,8 @@ package knightworld;
 
 import tileengine.TERenderer;
 import tileengine.TETile;
+import tileengine.Tileset;
+
 
 /**
  * Draws a world consisting of knight-move holes.
@@ -9,13 +11,50 @@ import tileengine.TETile;
 public class KnightWorld {
 
     private TETile[][] tiles;
-    // TODO: Add additional instance variables here
 
     public KnightWorld(int width, int height, int holeSize) {
-        // TODO: Fill in this constructor and class, adding helper methods and/or classes as necessary to draw the
-        //  specified pattern of the given hole size for a window of size width x height. If you're stuck on how to
-        //  begin, look at the provided demo code!
+        tiles = new TETile[width][height];
+        fill();
+        knightmovements();
+        holes(holeSize);
     }
+    private void fill() {
+        for (int x = 0; x < tiles.length; x++) {
+            for (int y = 0; y < tiles[0].length; y++) {
+                tiles[x][y] = Tileset.WALL;
+            }
+        }
+    }
+    private int[][] movements;
+
+    private void knightmovements() {
+        movements = new int[][] {{1, 0}, {4, 1}, {2, 2}, {0, 3}, {3, 4}};
+    }
+
+    private void holes(int holeSize) {
+
+        for (int x = 0; x < tiles.length; x += holeSize * 5) {
+            for (int y = 0; y < tiles[0].length; y += holeSize * 5) {
+                for (int[] cords : movements) {
+                    int xcord = holeSize * cords[0] + x;
+                    int ycord = holeSize * cords[1] + y;
+                    squares(xcord, ycord, holeSize);
+                }
+            }
+        }
+    }
+
+
+
+    private void squares(int startX, int startY, int size) {
+        for (int x = startX; x < startX + size && x < tiles.length; x++) {
+            for (int y = startY; y < startY + size && y < tiles[0].length; y++) {
+
+                tiles[x][y] = Tileset.NOTHING; // representing the hole
+            }
+        }
+    }
+
 
     /** Returns the tiles associated with this KnightWorld. */
     public TETile[][] getTiles() {
@@ -24,9 +63,9 @@ public class KnightWorld {
 
     public static void main(String[] args) {
         // Change these parameters as necessary
-        int width = 50;
-        int height = 30;
-        int holeSize = 2;
+        int width = 60;
+        int height = 40;
+        int holeSize = 4;
 
         KnightWorld knightWorld = new KnightWorld(width, height, holeSize);
 
--- lab11/src/project3prep.md
+++ lab11/src/project3prep.md
@@ -2,22 +2,24 @@
 
 **What distinguishes a hallway from a room? How are they similar?**
 
-Answer:
+Answer: A hallway is an area that is small and has curves that connect to a room. A room is a big open space that can have a door in it. They are similar as they both use walls and floors.
 
 **Can you think of an analogy between the process of 
 drawing a knight world and randomly generating a world 
 using rooms and hallways?**
 
-Answer:
+Answer: We can connect the rooms through knightworld wtih the knights movement patters, and "L". For randomworld, we would just have to make rooms in random areas and then connect them through random paths.
+
 
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually 
 get to the full knight world.**
 
-Answer:
+Answer: Fill in the world with blanks to get a set size. Afterwards, we would want to get a starting point and branch out from there.
+
 
 **This question is open-ended. Write some classes 
 and methods that might be useful for Project 3. Think 
 about what helper methods and classes you used for the lab!**
 
-Answer:
+Answer: We could use methods that make a certain area, connect rooms, and a class function of a room, and hallway.
