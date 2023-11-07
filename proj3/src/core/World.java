7ff65497ce8b60cde8b3711e516314f9f50d858d
Ryan-Nguyen <rnd.ngo@berkeley.edu>
2023-11-06 22:05:54 -0800

    done

--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -1,7 +1,34 @@
 package core;
 
+import tileengine.TETile;
+
 public class World {
 
     // build your own world!
+    int rooms;
+    int hallways;
+    int width;
+    int height;
+    TETile[][] world;
+
+
+    public World(int width,int height) {
+        this.height = height;
+        this.width = width;
+        world = new TETile[width][height];
+
+    }
+
+    private class Rooms {
+        public Rooms() {
+
+        }
+    }
+
+    private class Hallways {
+
+    }
+
+
 
 }
